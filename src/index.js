98dd500a2a2f689a114e28a967a2a22275cf6e78
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-02-24 20:24:56 +0200

    fix: change cod;

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (arr) {
+	let arr2 = [];
+	let i=1;
+	if(arr == undefined || arr.length == 0) return [];
+	else {
+while(i<arr.length){
+	arr[i].reverse();
+	i+=2;
+}
+arr2 = arr.join(',').split(',');
+arr = arr2.map(item => Number(item));
+console.log(arr,arr2);
+return arr;
+	}
 }
